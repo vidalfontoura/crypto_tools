cceb26ac3687c1b1eeccd3d79b716288dfb6441d
vfontoura <vfontoura@kenzanmedia.com>
2017-06-21 23:31:43 -0300

    Adding config

---
+++ claymore_config_nanopool/start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eth-us-west1.nanopool.org:9999 -ewal 0xcda77e77789e8d1181922028520066233ff5f700.ermacrig2/ermacxr@gmail.com -epsw x -mode 1 -ftime 10
